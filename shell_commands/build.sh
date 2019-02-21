d8ab314605f91295d5e123ee0602703dc3b2f8fc
saulocamposi <saulocamposi@gmail.com>
2019-02-21 02:08:53 +0000

    Fix build

--- shell_commands/build.sh
+++ shell_commands/build.sh
@@ -2,6 +2,4 @@
 
 
 docker stop `docker ps -q`
-docker build . -t apachedocker
-
-#&& docker run --name lara_apache -v $(pwd):/var/www/html/ -p 5002:80 -d apachedocker
+docker build . -t apachedocker && docker run --name lara_apache -v $(pwd):/var/www/html/ -p 5002:80 -d apachedocker
